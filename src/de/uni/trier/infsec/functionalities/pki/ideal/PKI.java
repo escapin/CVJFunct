70bea838a41e2707599acbccac1b46cfb0bc6513
Tomasz Truderung <ttruderung@gmail.com>
2012-12-05 11:00:04 +0100

    working with PKI

--- src/de/uni/trier/infsec/functionalities/pki/ideal/PKI.java
+++ src/de/uni/trier/infsec/functionalities/pki/ideal/PKI.java
@@ -5,7 +5,6 @@ import static de.uni.trier.infsec.utils.MessageTools.equal;
 import static de.uni.trier.infsec.utils.MessageTools.getZeroMessage;
 import de.uni.trier.infsec.environment.crypto.CryptoLib;
 import de.uni.trier.infsec.environment.crypto.KeyPair;
-import de.uni.trier.infsec.functionalities.pki.real.PKI.Encryptor;
 import de.uni.trier.infsec.utils.MessageTools;
 
 /**
@@ -14,56 +13,67 @@ import de.uni.trier.infsec.utils.MessageTools;
 public class PKI {
 	
 /// The public interface ///
-
+	
+	/** An object encapsulating the public key of some party.
+	 *  
+	 *  This key can be accessed directly of indirectly via method encrypt.
+	 *  Method 'encrypt' realizes the "ideal" encryption, where a string of 
+	 *  zeros is encrypted instead of the original message and the pair 
+	 *  (plaintext, ciphertest) is stored in a log which can be then used
+	 *  for decryption.    
+	 */
 	static public class Encryptor {
 		private byte[] ID;	
 		private byte[] publicKey;
-		private MessagePairList log;
+		private EncryptionLog log;
 
-		private Encryptor(byte[] id, byte[] publicKey, MessagePairList log) {
+		private Encryptor(byte[] id, byte[] publicKey, EncryptionLog log) {
 			this.ID = id;
 			this.publicKey = publicKey;
 			this.log = log;
 		}
+		
 		public byte[] encrypt(byte[] message) {
-			byte[] messageCopy = copyOf(message);
 			byte[] randomCipher = null;
 			// keep asking the environment for the ciphertext, until a fresh one is given:
-			while( randomCipher==null || log.contains(randomCipher) ) {
+			while( randomCipher==null || log.containsCiphertext(randomCipher) ) {
 				randomCipher = copyOf(CryptoLib.pke_encrypt(getZeroMessage(message.length), copyOf(publicKey)));
 			}
-			log.add(messageCopy, randomCipher);
+			log.add(copyOf(message), randomCipher);
 			return copyOf(randomCipher);
 		}
 		public byte[] getPublicKey() {
-			return publicKey;
+			return copyOf(publicKey);
 		}
 	}
 	
-	
+	/** An object encapsulating the private and public keys of some party. */
 	static public class Decryptor {
 		private byte[] ID;
 		private byte[] publicKey;
 		private byte[] privateKey;
-		private MessagePairList log;
+		private EncryptionLog log;
 
 		private Decryptor(byte[] id) {
 			KeyPair keypair = CryptoLib.generateKeyPair();
-			byte[] privKey = copyOf(keypair.privateKey);
-			byte[] publKey = copyOf(keypair.publicKey);
+			this.privateKey = copyOf(keypair.privateKey);
+			this.publicKey = copyOf(keypair.publicKey);
 			this.ID = id;
-			this.publicKey = publKey;
-			this.privateKey = privKey;
-			this.log = null;
+			this.log = new EncryptionLog();
 		}		
+		
+		/** "Decrypts" a message by, first trying to find in in the log (and returning
+		 *   the related plaintext) and, only if this fails, by using real decryption. */
 		public byte[] decrypt(byte[] message) {
 			byte[] messageCopy = copyOf(message); 
-			if (!log.contains(messageCopy)) {
+			if (!log.containsCiphertext(messageCopy)) {
 				return copyOf( CryptoLib.pke_decrypt(copyOf(privateKey), messageCopy) );
 			} else {
 				return copyOf( log.lookup(messageCopy) );
 			}			
 		}
+		
+		/** Returns a new encryptor object sharing the same public key, ID, and log. */
 		public Encryptor getEncryptor() {
 			return new Encryptor(ID, publicKey, log);
 		}	
@@ -74,7 +84,7 @@ public class PKI {
 		if( handlers.fetch(id) != null ) return null; // a party with this id has already registered
 		Decryptor decryptor = new Decryptor(id);
 		Encryptor encryptor = decryptor.getEncryptor();
-		handlers = new EncrList(encryptor, handlers);
+		handlers.add(encryptor);
 		return decryptor;
 	}
 	
@@ -84,58 +94,63 @@ public class PKI {
 	
 	
 /// Implementation ///
-	
-	private static class EncrList {	
-		Encryptor encryptor;
-		EncrList next;
-	
-		EncrList(Encryptor encryptor, EncrList next) {
-			this.encryptor= encryptor;
-			this.next = next;
-		}
 		
-		Encryptor fetch(byte[] ID) {
-			for( EncrList node = this;  node!=null;  node = node.next ) {
-				if( equal(ID, node.encryptor.ID) )
-					return node.encryptor;
-			}
-			return null;
-		}
-	}
-	
-	private static class MessagePair {
+	private static class MessagePairList {
 		byte[] ciphertext;
 		byte[] plaintext;
-		MessagePair next;
-		public MessagePair(byte[] ciphertext, byte[] plaintext, MessagePair next) {
+		MessagePairList next;
+		public MessagePairList(byte[] ciphertext, byte[] plaintext, MessagePairList next) {
 			this.ciphertext = ciphertext;
 			this.plaintext = plaintext;
 			this.next = next;
 		}
 	}
 	
-	private static class MessagePairList {
-		private MessagePair first = null;
+	private static class EncryptionLog {
+		private MessagePairList first = null;
 		
-		public void add(byte[] pTxt, byte[] cTxt) {
-			first = new MessagePair(cTxt, pTxt, first);
+		public void add(byte[] plaintext, byte[] ciphertext) {
+			first = new MessagePairList(ciphertext, plaintext, first);
 		}
 
-	    byte[] lookup(byte[] ciphertext) {
-	        MessagePair tmp = first;
-	        while( tmp != null ) {
-	            if( MessageTools.equal(tmp.ciphertext, ciphertext) )
-	                return tmp.plaintext;
-	            tmp = tmp.next;
-	        }
+	    byte[] lookup(byte[] ciphertext) {	    	
+	    	for( MessagePairList node = first;  node != null;  node = node.next ) {
+	            if( MessageTools.equal(node.ciphertext, ciphertext) )
+	                return node.plaintext;	    		
+	    	}
 	        return null;
 	    }
 	    
-	    boolean contains(byte[] ciphertext) {
+	    boolean containsCiphertext(byte[] ciphertext) {
 	    	return lookup(ciphertext) != null;
 	    }    
 	}
-	
-	private static EncrList handlers = null;	
 
+		
+	private static class EncrList {
+		Encryptor encryptor;
+		EncrList  next;
+		EncrList(Encryptor encryptor, EncrList next) {
+			this.encryptor= encryptor;
+			this.next = next;
+		}
+	}
+	
+	private static class Handlers {	
+		private EncrList first = null;
+		
+		public void add(Encryptor encr) {
+			first = new EncrList(encr, first);
+		}
+		
+		Encryptor fetch(byte[] ID) {
+			for( EncrList node = first;  node != null;  node = node.next ) {
+				if( equal(ID, node.encryptor.ID) )
+					return node.encryptor;
+			}
+			return null;
+		}
+	}
+
+	private static Handlers handlers = new Handlers();	
 }
--- src/de/uni/trier/infsec/functionalities/pki/real/PKI.java
+++ src/de/uni/trier/infsec/functionalities/pki/real/PKI.java
@@ -4,49 +4,80 @@ import static de.uni.trier.infsec.utils.MessageTools.copyOf;
 import de.uni.trier.infsec.environment.crypto.CryptoLib;
 import de.uni.trier.infsec.environment.crypto.KeyPair;
 
-
-
-
 /**
  * Real functionality for PKI (Public Key Infrastructure).
+ * 
+ * The intended usage:
+ * 
+ * To encrypt messages for a party with identifier id_of_A: 
+ * 		Encryptor encryptor_of_A = PKI.getEncryptor(id_A);
+ * 		byte[] encrypted1 = encryptor_of_A.encrypt(message1);
+ * 		byte[] encrypted2 = encryptor_of_A.encrypt(message2);
+ * 
+ * To register with my_id:
+ * 		Decryptor my_decryptor = PKI.register(my_id);
+ * 		if( my_decryptor == null ) 
+ * 			//  somebody has already registered using my_id...
+ * 		else
+ * 			byte[] message = my_decryptor.decrypt(ciphertext)
+ * 	
+ *	The serialization methods (decryptorToBytes, decryptorFromBytes)
+ *	can be used to store/restore a decryptor.
  */
 public class PKI {
 
 /// The public interface ///
 	
+	/** An object encapsulating the public key of some party. 
+	 *  This key can be accessed directly of indirectly via method encrypt.  
+	 */
 	static public class Encryptor {
 		private byte[] publicKey;	
+		
 		private Encryptor(byte[] publicKey) {
 			this.publicKey = publicKey;
 		}
+		
 		public byte[] encrypt(byte[] message) {
 			return copyOf(CryptoLib.pke_encrypt(copyOf(message), copyOf(publicKey)));		
 		}
+		
 		public byte[] getPublicKey() {
-			return publicKey;
+			return copyOf(publicKey);
 		}
 	}
 	
+	/** An object encapsulating the private and public keys of some party. */
 	static public class Decryptor {
 		private byte[] publicKey;
 		private byte[] privateKey;
+		
 		private Decryptor(byte[] publicKey, byte[] privateKey) {
 			this.publicKey = publicKey;
 			this.privateKey = privateKey;
 		}
+		
+		/** Decrypts 'message' with the encapsulated private key. */
 		public byte[] decrypt(byte[] message) {
 			return copyOf(CryptoLib.pke_decrypt(copyOf(message), copyOf(privateKey)));
 		}	
+		
+		/** Returns a new encryptor object with the same public key. */
 		public Encryptor getEncryptor() {
 			return new Encryptor(publicKey);
 		}
 	}
 		
+	/** Registers a user with the given id. 
+	 * 
+	 *   It fails (returns null) if this id has been already registered. Otherwise, it creates
+	 *   new decryptor (with fresh public/private keys) and registers it under the given id. 
+	 */
 	public static Decryptor register(byte[] id) {
 		KeyPair keypair = CryptoLib.generateKeyPair();
 		byte[] privateKey = copyOf(keypair.privateKey);
 		byte[] publicKey = copyOf(keypair.publicKey);  
-		if( !pki_register(id, publicKey) ) return null; // registration has not succeeded
+		if( !pki_register(copyOf(id), copyOf(publicKey)) ) return null; // registration has not succeeded (id already used)
 		return new Decryptor(publicKey, privateKey);
 	}
 	
@@ -58,10 +89,19 @@ public class PKI {
 	}
 		
 
+/// Extended interface (not in the ideal functionality): serialization/deserialization of decryptors ///
+	
+	public static byte[] decryptorToBytes(Decryptor decryptor)
+		{ return null; }  // TODO
+	
+	public Decryptor decryptorFromBytes(byte[] bytes)
+		{ return null; }  // TODO
+	
 /// Implementation ///
 	
 	private static boolean pki_register(byte[] id, byte[] pubKey)
-		{return false;}
+		{return false;}  // TODO
+	
 	private static byte[] pke_getPublicKey(byte[] id)
-		{return null;}
+		{return null;}	 // TODO
 }
