25a6d086a960da066e02fe8380ad790b84ac622d
Tomasz Truderung <ttruderung@gmail.com>
2013-01-21 15:58:40 +0100

    change the interface of Network

---
+++ src/de/uni/trier/infsec/lib/network/Client.java
@@ -0,0 +1,12 @@
+package de.uni.trier.infsec.lib.network;
+
+public class Client {
+	/*
+	 * Sends message to the given server/port and gets the response
+	 * (also a message).
+	 */
+	byte[] send(byte[] message, String server, int port) throws NetworkError {
+		return null;
+		// TODO
+	}
+}
