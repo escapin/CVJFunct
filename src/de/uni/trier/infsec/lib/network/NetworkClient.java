6aead692cd0191450638ac51e28e5bb017dfe4d4
Tomasz Truderung <ttruderung@gmail.com>
2013-01-24 11:22:14 +0100

    small issues in networking

--- src/de/uni/trier/infsec/lib/network/NetworkClient.java
+++ src/de/uni/trier/infsec/lib/network/NetworkClient.java
@@ -6,12 +6,30 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class NetworkClient {
-	/*
+	
+	/**
+	 * Sends message to the given server/port (without waiting for any response)
+	 */
+	public static void send(byte[] message, String server, int port) throws NetworkError {
+		Socket s = null;
+		try {
+			s = new Socket(server, port);
+			OutputStream os = s.getOutputStream();			
+			os.write(message);
+			os.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new NetworkError();
+		} finally {
+			try { s.close(); } catch (Exception e) {}
+		}
+	}
+
+	/**
 	 * Sends message to the given server/port and gets the response
 	 * (also a message).
 	 */
-	
-	public static byte[] send(byte[] message, String server, int port) throws NetworkError {
+	public static byte[] sendRequest(byte[] message, String server, int port) throws NetworkError {
 		Socket s = null;
 		try {
 			s = new Socket(server, port);
@@ -24,7 +42,9 @@ public class NetworkClient {
 			ByteArrayOutputStream buffer = new ByteArrayOutputStream(); // we can handle Messages up to 1K
 			byte[] bufferArr = new byte[512];
 			
-			buffer.write(bufferArr, 0, is.read(bufferArr)); // This first call is necessary for the call to become blocking! TODO: Timeout needed?
+			buffer.write(bufferArr, 0, is.read(bufferArr)); // This first call is necessary for the call to become blocking!
+				// XXX: I do not quite get it (which is ok:)), but why you do not use this in the server's code?
+			// TODO: Timeout needed? -- yes, I thing it should give up after some reasonable time.
 			while (is.available() > 0) buffer.write(bufferArr, 0, is.read(bufferArr));
 			return buffer.toByteArray();
 			
@@ -35,6 +55,8 @@ public class NetworkClient {
 			try { s.close(); } catch (Exception e) {}
 		}
 	}
+	
+	
 	
 //	public static void main(String[] args) throws NetworkError {
 //		byte[] out = send(new byte[] {(byte) 0x99,  (byte) 0x88}, "127.0.0.1", 7070);
--- src/de/uni/trier/infsec/lib/network/NetworkServer.java
+++ src/de/uni/trier/infsec/lib/network/NetworkServer.java
@@ -11,19 +11,12 @@ import java.util.Hashtable;
 
 public class NetworkServer {
 	
-	// I can imagine that we could have a single-threaded implementation of this class, but
-	// that could be awkward. Probably it would be better to have multi-threaded implementation,
-	// with separate threads for accepting client connections and for waiting for messages and
-	// putting those messages along with the sockets in some queue, from which method 'response'
-	// would take requests. In any case, however, we assume that methods 'nextRequest' and 'response'
-	// are always called from one (main) thread (so the notion of 'the last request' makes sense).
-	
-	// --> Now we have one Thread listening for connections and one Thread for each connection (reading the message).
+	// Now we have one Thread listening for connections and one Thread for each connection (reading the message).
 	// The Messages get cached within a queue and every call to nextRequest returns the next element.
 	
 	private static final int LISTEN_PORT = 7070; // Default listening port. After connection is established, there will be another port used for communication!
 	private static Hashtable<Socket, byte[]> queue = new Hashtable<>(); // Not HashMap because Hashtable is Thread-safe!
-	private static Socket current = null; // Socket of the current message. Needed to answere the last request which has been processed
+	private static Socket current = null; // Socket of the current message. Needed to answer the last request which has been processed
 	
 	static Thread listenThread = null; // Thread which is listening for new connections
 	
@@ -31,12 +24,15 @@ public class NetworkServer {
 	 * Returns the next requests (a message).
 	 */
 	public static byte[] nextRequest() throws NetworkError {
+		// start the listener thread if not started yet
 		if (listenThread == null) { // TODO: Start server manually or like this (within first call of "nextRequest"?
+			                        // --> seems ok for me as it is.
 			Runnable listenRunnable = new ListenThread();
 			listenThread = new Thread(listenRunnable);
 			listenThread.start();
 		}
 		
+		// get the next message from the queue (null if there is none)
 		if (queue.keys().hasMoreElements()) {
 			current = queue.keys().nextElement();
 			byte[] msg = queue.get(current);
@@ -51,6 +47,7 @@ public class NetworkServer {
 	 * sent the last request obtained by calling method 'nextRequest').
 	 */
 	public static void response(byte[] message) throws NetworkError {
+		// XXX: does it works fine with message==null?
 		OutputStream os;
 		try {
 			os = current.getOutputStream();
@@ -98,6 +95,8 @@ public class NetworkServer {
 				ByteArrayOutputStream buffer = new ByteArrayOutputStream(); // we can handle Messages up to 1K
 				byte[] bufferArr = new byte[512];
 				while (is.available() > 0) buffer.write(bufferArr, 0, is.read(bufferArr));
+					// XXX: doesn't this loop uses the processor?
+					// How does it work? how do we know that the message ends? 
 				queue.put(mysock, buffer.toByteArray()); // Put the received message to the queue. Socket is kept open!
 			} catch (IOException e) {
 				e.printStackTrace();
