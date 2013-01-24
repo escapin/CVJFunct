b0aaa08d452db32ad7fdcabfece77af2f7f7ec4c
Tomasz Truderung <ttruderung@gmail.com>
2013-01-24 14:20:39 +0100

    woking on the SAMT functionalities and the simulator

--- src/de/uni/trier/infsec/environment/smt/SAMTEnv.java
+++ src/de/uni/trier/infsec/environment/smt/SAMTEnv.java
@@ -1,172 +1,37 @@
 package de.uni.trier.infsec.environment.smt;
 
-import de.uni.trier.infsec.functionalities.pki.real.PKIError;
-import de.uni.trier.infsec.functionalities.samt.real.SAMT;
-import de.uni.trier.infsec.functionalities.samt.real.SAMT.AgentProxy;
-import de.uni.trier.infsec.functionalities.samt.real.SAMT.Channel;
-import de.uni.trier.infsec.lib.network.NetworkError;
-import de.uni.trier.infsec.utils.MessageTools;
 
-/**
- * Simulator for ideal SAMT.
- *
- * This interface is implemented by the simulator in the realization proof.
- */
+import de.uni.trier.infsec.environment.Environment;
+
+
 public class SAMTEnv {
 
 	public static void register(int id)	{
-		try {
-			AgentProxy proxy = SAMT.register(id);
-			agentProxies.add(proxy);
-		}
-		catch (PKIError e) {}
+		Environment.untrustedOutput(7801);
+		Environment.untrustedOutput(id);
 	}
 
 	public static boolean channelTo(int sender_id, int recipient_id, String server, int port) {
-		AgentProxy sender = agentProxies.fetch(sender_id);
-		try {
-			Channel channel = sender.channelTo(recipient_id, server, port);
-			channels.add(channel, sender_id, recipient_id, server, port);
-		}
-		catch (PKIError e) {}
-		catch (NetworkError e) {
-			return false;
-		}
-		return true;
+		Environment.untrustedOutput(7802);
+		Environment.untrustedOutput(sender_id);
+		Environment.untrustedOutput(recipient_id);
+		Environment.untrustedOutputString(server);
+		Environment.untrustedOutput(port);
+		return Environment.untrustedInput()==0;
 	}
 
-	public static boolean send(int message_length, int sender_id, int recipient_id, String server, int port) {
-		Channel channel = channels.fetch(sender_id, recipient_id, server, port);
-		try {
-			// do the simulation (for a message of the same length)
-			byte[] message = MessageTools.getZeroMessage(message_length);
-			byte[] output_message = channel.send(message);
-			// and additionally, record this output message (used in getMessage)
-			agentProxies.getMessageQueue(recipient_id).add(output_message);
-			return true;
-		}
-		catch (NetworkError e) {
-			return false;
-		}
+	public static void send(int message_length, int sender_id, int recipient_id, String server, int port) {
+		Environment.untrustedOutput(7803);
+		Environment.untrustedOutput(message_length);
+		Environment.untrustedOutput(sender_id);
+		Environment.untrustedOutput(recipient_id);
+		Environment.untrustedOutputString(server);
+		Environment.untrustedOutput(port);
 	}
 
 	public static int getMessage(int id) {
-		AgentProxy proxy = agentProxies.fetch(id);
-		SAMT.AuthenticatedMessage am = proxy.getMessage();
-		if( am == null ) return -1; // no message
-		int index = agentProxies.getMessageQueue(id).getIndex(am.raw_input);
-		return index;
+		Environment.untrustedOutput(7804);
+		Environment.untrustedOutput(id);
+		return Environment.untrustedInput();
 	}
-
-
-	////////////////////////////////////////////////////////////////////////////////////
-
-	private static class MessageQueue 
-	{
-		private static class Node {
-			final byte[] message;
-			final Node next;
-
-			Node(byte[] message, Node next) {
-				this.message = message;
-				this.next = next;
-			}
-		}		
-		private Node first = null;
-
-		void add(byte[] message) {
-			first = new Node(message, first);
-		}
-
-		int getIndex(byte[] message) {
-			Node node = first;
-			for( int i=0; node!=null;  ++i ) {
-				if( MessageTools.equal(message, node.message) )
-					return i;
-				node = node.next;
-			}
-			return -1;
-		}
-	}
-
-	private static class ProxyList
-	{
-		static class Node {
-			final AgentProxy agent;
-			final Node  next;
-			MessageQueue message_queue;
-			Node(AgentProxy agent, Node next) {
-				this.agent = agent;
-				this.next = next;
-				this.message_queue = new MessageQueue();
-			}
-		}
-		private Node first = null;
-
-		public void add(AgentProxy agent) {
-			first = new Node(agent, first);
-		}
-
-		AgentProxy fetch(int id) {
-			for( Node node = first;  node != null;  node = node.next )
-				if( id == node.agent.ID )
-					return node.agent;
-			return null;
-		}
-
-		MessageQueue getMessageQueue(int id) {
-			for( Node node = first;  node != null;  node = node.next )
-				if( id == node.agent.ID )
-					return node.message_queue;
-			return null;			
-		}
-	}
-
-	private static ProxyList agentProxies;
-
-	private static class ChannelInfo {
-		final int sender_id;
-		final int recipient_id;
-		final String server;
-		final int port;
-
-		ChannelInfo(int sender_id, int recipient_id, String server, int port) {
-			this.sender_id = sender_id;
-			this.recipient_id = recipient_id;
-			this.server = server;
-			this.port = port;
-		}
-	}
-
-	private static class ChannelList
-	{
-		private static class Node {
-			final Channel channel;
-			final ChannelInfo chinf;
-			final Node  next;
-			Node(Channel channel, ChannelInfo chinf, Node next) {
-				this.channel = channel;
-				this.chinf = chinf;
-				this.next = next;
-			}
-		}
-		private Node first = null;
-
-		public void add(Channel channel, int sender_id, int recipient_id, String server, int port) {
-			ChannelInfo chinf = new ChannelInfo(sender_id, recipient_id, server, port);
-			first = new Node(channel, chinf, first);
-		}
-
-		Channel fetch(int sender_id, int recipient_id, String server, int port) {
-			for( Node node = first;  node != null;  node = node.next ) {
-				ChannelInfo ci = node.chinf;
-				if( sender_id==ci.sender_id && recipient_id==ci.recipient_id && server.equals(ci.server) && port==ci.port )
-					return node.channel;
-			}
-			return null;
-		}
-	}
-
-	private static ChannelList channels;
 }
-
--- src/de/uni/trier/infsec/functionalities/samt/real/SAMT.java
+++ src/de/uni/trier/infsec/functionalities/samt/real/SAMT.java
@@ -16,6 +16,8 @@ public class SAMT {
 
 	//// The public interface ////
 
+	static public class SAMTError extends Exception {}
+
 	/** 
 	 * Pair message, sender_id. 
 	 *
@@ -24,9 +26,9 @@ public class SAMT {
 	static public class AuthenticatedMessage {
 		public byte[] message;
 		public int sender_id;
-		public byte[] raw_input;
+		public byte[] raw_input; // FIXME: remove from this variant
 
-		public AuthenticatedMessage(byte[] message, int sender, byte[] raw_input) {
+		private AuthenticatedMessage(byte[] message, int sender, byte[] raw_input) {
 			this.sender_id = sender;  this.message = message;  this.raw_input = raw_input;
 		}
 	}
@@ -54,7 +56,8 @@ public class SAMT {
 			this.signer = signer;
 		}
 
-		public AuthenticatedMessage getMessage() {
+		public AuthenticatedMessage getMessage() throws SAMTError {
+			if (registrationInProgress) throw new SAMTError();
 			try {
 				byte[] inputMessage = NetworkServer.read();
 				// get the sender id and her verifier
@@ -79,7 +82,8 @@ public class SAMT {
 			// to ill-formed messages.
 		}
 
-		public Channel channelTo(int recipient_id, String server, int port) throws PKIError, NetworkError {
+		public Channel channelTo(int recipient_id, String server, int port) throws SAMTError, PKIError, NetworkError {
+			if (registrationInProgress) throw new SAMTError();
 			PKIEnc.Encryptor recipient_encryptor = PKIEnc.getEncryptor(recipient_id);
 			return new Channel(this.ID, this.signer, recipient_encryptor, server, port);
 		}
@@ -88,7 +92,7 @@ public class SAMT {
 	/**
 	 * Objects representing secure and authenticated channel from sender to recipient. 
 	 * 
-	 * Such objects allow one to securely send a message to the recipient, where the 
+	 * Such objects allow one to securely send a message to the recipient, where the
 	 * sender is authenticated to the recipient.
 	 */
 	static public class Channel 
@@ -107,16 +111,17 @@ public class SAMT {
 			this.port = port;
 		}		
 
-		public byte[] send(byte[] message) throws NetworkError {
+		public byte[] send(byte[] message) {
 			// sign and encrypt
 			byte[] signature = sender_signer.sign(message);
 			byte[] signed = MessageTools.concatenate(signature, message);
 			byte[] signedAndEncrypted = recipient_encryptor.encrypt(signed);
 			byte[] sender_id_as_bytes = MessageTools.intToByteArray(sender_id);
 			byte[] outputMessage = MessageTools.concatenate(sender_id_as_bytes, signedAndEncrypted);
-			NetworkClient.send(outputMessage, server, port);
-			return outputMessage; // used by the simulator for SAMT
-			// TODO: can we assume that messages at each step are not null?
+			try {
+				NetworkClient.send(outputMessage, server, port);
+			} catch (NetworkError e) {}
+			return outputMessage; // used by the simulator for SAMT // FIXME: remove from this variant
 		}
 	}
 
@@ -127,13 +132,12 @@ public class SAMT {
 	 * We assume that the registration is not blocked, that is it does not ends successfully only
 	 * if the given id has been already used (but not because of some network problems).
 	 */	
-	public static AgentProxy register(int id) throws PKIError {
-		if( registrationInProgress ) return null;
+	public static AgentProxy register(int id) throws SAMTError, PKIError {
+		if (registrationInProgress) throw new SAMTError();
 		registrationInProgress = true;
 		try {
 			PKIEnc.Decryptor decryptor = PKIEnc.register(id);
 			PKISig.Signer signer = PKISig.register(id);
-
 			registrationInProgress = false;
 			return new AgentProxy(id, decryptor, signer);
 		}
