f5834f29925b8247a724f7f1ee122694932519b9
Tomasz Truderung <ttruderung@gmail.com>
2013-02-01 15:59:55 +0100

    implement the new interface for environmnent.network

---
+++ src/de/uni/trier/infsec/environment/network/NetworkServer.java
@@ -0,0 +1,31 @@
+package de.uni.trier.infsec.environment.network;
+
+import de.uni.trier.infsec.environment.Environment;
+import de.uni.trier.infsec.lib.network.NetworkError;
+
+public class NetworkServer {
+
+	public static byte[] nextRequest() throws NetworkError {
+		// input
+		Environment.untrustedOutput(0x2401);
+		// output
+		if ( Environment.untrustedInput()==0 ) throw new NetworkError();
+		return Environment.untrustedInputMessage();
+	}
+
+	public static void response(byte[] message) throws NetworkError {
+		// input
+		Environment.untrustedOutput(0x2402);
+		Environment.untrustedOutputMessage(message);
+		// output
+		if ( Environment.untrustedInput()==0 ) throw new NetworkError();		
+	}
+
+	public static byte[] read() throws NetworkError {
+		// input
+		Environment.untrustedOutput(0x2403);
+		// output
+		if ( Environment.untrustedInput()==0 ) throw new NetworkError();
+		return Environment.untrustedInputMessage();		
+	}
+}
