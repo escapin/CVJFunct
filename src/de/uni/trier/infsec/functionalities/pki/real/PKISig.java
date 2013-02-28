0ec8e0059c9be8fa5859e3d2567c93fe76f00256
Tomasz Truderung <ttruderung@gmail.com>
2013-02-28 12:07:30 +0100

    PKI as a separate sub-functionality in real PKISig

--- src/de/uni/trier/infsec/functionalities/pki/real/PKISig.java
+++ src/de/uni/trier/infsec/functionalities/pki/real/PKISig.java
@@ -4,6 +4,8 @@ import static de.uni.trier.infsec.utils.MessageTools.concatenate;
 import static de.uni.trier.infsec.utils.MessageTools.copyOf;
 import static de.uni.trier.infsec.utils.MessageTools.first;
 import static de.uni.trier.infsec.utils.MessageTools.second;
+import static de.uni.trier.infsec.utils.MessageTools.intToByteArray;
+import static de.uni.trier.infsec.utils.MessageTools.byteArrayToInt;
 import de.uni.trier.infsec.lib.crypto.CryptoLib;
 import de.uni.trier.infsec.lib.crypto.KeyPair;
 import de.uni.trier.infsec.lib.network.NetworkError;
@@ -23,9 +25,11 @@ public class PKISig {
 	 * a signature.
 	 */
 	static public class Verifier {
+		public final int ID;
 		private byte[] verifKey;
 
-		private Verifier(byte[] verifKey) {
+		Verifier(int id, byte[] verifKey) {
+			this.ID = id;
 			this.verifKey = verifKey;
 		}
 
@@ -43,13 +47,14 @@ public class PKISig {
 	 * create signatures.
 	 */
 	static public class Signer {
+		public final int ID;
 		private byte[] verifKey;
 		private byte[] signKey;
 
-		private Signer() {
-			KeyPair keypair = CryptoLib.generateSignatureKeyPair();
-			this.signKey = copyOf(keypair.privateKey);
-			this.verifKey = copyOf(keypair.publicKey);
+		private Signer(int id, byte[] verifKey, byte[] signKey) {
+			this.ID = id;
+			this.verifKey = verifKey;
+			this.signKey = signKey;
 		}
 
 		public byte[] sign(byte[] message) {
@@ -58,48 +63,38 @@ public class PKISig {
 		}
 
 		public Verifier getVerifier() {
-			return new Verifier(verifKey);
+			return new Verifier(ID, verifKey);
 		}
 	}
 
-	public static Signer register(int id, byte[] domain) throws NetworkError, PKIError {
-		Signer signer = new Signer();
-		pki_server.register(id, copyOf(domain), copyOf(signer.verifKey));		
+	public static Signer register(int id, byte[] domain) throws NetworkError, PKIError {		
+		KeyPair keypair = CryptoLib.generateSignatureKeyPair();
+		byte[] signKey = copyOf(keypair.privateKey);
+		byte[] verifKey = copyOf(keypair.publicKey);
+		Signer signer = new Signer(id, verifKey, signKey);
+		Verifier verifier = signer.getVerifier();
+		PKIForSig.register(verifier, domain);		
 		return signer;
 	}
 
 	public static Verifier getVerifier(int id, byte[] domain) throws NetworkError, PKIError {
-		byte[] verKey = pki_server.getKey(id, domain);		
-		return new Verifier(verKey);
+		return PKIForSig.getVerifier(id, domain);
 	}
-	
-	private static boolean remoteMode = Boolean.parseBoolean(System.getProperty("remotemode"));
-	private static PKIServerInterface pki_server = null;
-	static {
-		if(remoteMode) {
-			pki_server = new RemotePKIServer();
-			System.out.println("Working in remote mode");
-		} else {
-			pki_server = new PKIServerCore();
-			System.out.println("Working in local mode");
-		}
-	}
-	
+		
 	public static byte[] signerToBytes(Signer signer) {
+		byte[] id = intToByteArray(signer.ID);
         byte[] sign = signer.signKey;
         byte[] verify = signer.verifKey;
 
-        byte[] out = concatenate(sign, verify);
+        byte[] out = concatenate(id, concatenate(sign, verify));
         return out;
 	}
 	
 	public static Signer signerFromBytes(byte[] bytes) {
-        byte[] sign = first(bytes);
-        byte[] verify = second(bytes);
-
-        Signer signer = new Signer();
-        signer.signKey = sign;
-        signer.verifKey = verify;
-        return signer;
+		int id = byteArrayToInt(first(bytes));
+		byte[] rest = second(bytes);
+        byte[] sign_key = first(rest);
+        byte[] verif_key = second(rest);
+        return new Signer(id, verif_key, sign_key);
 	}		
 }
