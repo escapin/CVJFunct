d6af46c5338d90dca7cdc294d034ee15c37e8820
Enrico Scapin <scapin@uni-trier.de>
2014-02-21 14:55:13 +0100

    Modifing the SMT test

--- src/de/uni/trier/infsec/tests/TestSMT.java
+++ src/de/uni/trier/infsec/tests/TestSMT.java
@@ -9,12 +9,17 @@ import org.junit.Test;
 import de.uni.trier.infsec.functionalities.pki.PKI;
 import de.uni.trier.infsec.functionalities.pki.PKIServerCore;
 import de.uni.trier.infsec.functionalities.smt.SMT;
-import de.uni.trier.infsec.functionalities.smt.SMT.AgentProxy;
+//import de.uni.trier.infsec.functionalities.smt.SMT.AgentProxy;
 import de.uni.trier.infsec.functionalities.smt.SMT.AuthenticatedMessage;
-import de.uni.trier.infsec.functionalities.smt.SMT.Channel;
+//import de.uni.trier.infsec.functionalities.smt.SMT.Channel;
 import de.uni.trier.infsec.functionalities.smt.SMT.PKIError;
+import de.uni.trier.infsec.functionalities.smt.SMT.Receiver;
 import de.uni.trier.infsec.functionalities.smt.SMT.SMTError;
+import de.uni.trier.infsec.functionalities.smt.SMT.Sender;
+import static de.uni.trier.infsec.functionalities.smt.SMT.registerSender;
+import static de.uni.trier.infsec.functionalities.smt.SMT.registerReceiver;
 import de.uni.trier.infsec.lib.network.NetworkError;
+import de.uni.trier.infsec.lib.network.NetworkServer;
 import de.uni.trier.infsec.utils.Utilities;
 
 public class TestSMT extends TestCase {
@@ -26,28 +31,38 @@ public class TestSMT extends TestCase {
 	@Test
 	public void testSMT() throws PKIError, NetworkError, Exception, SMTError {
 		Process pr = null;
+		String server="localhost";
+		int testPort=7777;
 		try {
 			PKI.useLocalMode();
-
-			AgentProxy p1 = SMT.register(TEST_ID1);
-			AgentProxy p2 = SMT.register(TEST_ID2);
+			//PKI.useRemoteMode();
 			
-			p2 = SMT.agentFromBytes(SMT.agentToBytes(p2));
-			p1 = SMT.agentFromBytes(SMT.agentToBytes(p1));
+			Sender sender01 = registerSender(TEST_ID1);
+			Receiver receiver02 = registerReceiver(TEST_ID2);
 			
-			p2.getMessage(7777); // Starts listening for messages
+			// AgentProxy p1 = SMT.register(TEST_ID1);
+			// AgentProxy p2 = SMT.register(TEST_ID2);
 			
-			Channel c1 = p1.channelTo(TEST_ID2, "localhost", 7777);
-			c1.send(TEST_DATA);
-			Thread.sleep(5000);
-			AuthenticatedMessage msg = p2.getMessage(7777);
+			// p2 = SMT.agentFromBytes(SMT.agentToBytes(p2));
+			// p1 = SMT.agentFromBytes(SMT.agentToBytes(p1));
+			
+			// p2.getMessage(7777); // Starts listening for messages
+			
+			// Channel c1 = p1.channelTo(TEST_ID2, "localhost", 7777);
+			// c1.send(TEST_DATA);
+			NetworkServer.listenForRequests(testPort);
+			sender01.sendTo(TEST_DATA, TEST_ID2, "localhost", testPort);
+			Thread.sleep(500);
+			// AuthenticatedMessage msg = p2.getMessage(7777);
+			AuthenticatedMessage msg = receiver02.getMessage(testPort);
 			
 			System.out.println("REC " + Utilities.byteArrayToHexString(msg.message));
 			assertTrue("Received data is not equal to sent data", Utilities.arrayEqual(TEST_DATA, msg.message));
 			
 			boolean error = false;
 			try {
-				SMT.register(TEST_ID2);
+				//SMT.register(TEST_ID2);
+				registerSender(TEST_ID1); // 1:1 matching SenderObj:ID 
 			} catch (PKIError e) {
 				error = true;
 			}
@@ -55,8 +70,17 @@ public class TestSMT extends TestCase {
 			
 			error = false;
 			try {
-				p1.channelTo(TEST_ID2 + 1, "localhost", 7777);
+				//SMT.register(TEST_ID1);
+				registerReceiver(TEST_ID2); // 1:1 matching ReceiverObj:ID 
 			} catch (PKIError e) {
+				error = true;
+			}
+			assertTrue("Duplicate registration did not lead to an error", error);
+			
+			error = false;
+			try {
+				sender01.sendTo(TEST_DATA, TEST_ID2 + 1, "localhost", testPort);
+			} catch (NetworkError e) {
 				error = true;
 			}
 			assertTrue("Invalid request did not lead to an error", error);
