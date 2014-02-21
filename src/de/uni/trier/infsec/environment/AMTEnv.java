cb1caf515a68b441ba18e0bc006a943d90890667
Tomasz Truderung <ttruderung@gmail.com>
2014-02-21 11:31:21 +0100

    rewrite the AMT functionality

--- src/de/uni/trier/infsec/environment/AMTEnv.java
+++ src/de/uni/trier/infsec/environment/AMTEnv.java
@@ -2,21 +2,13 @@ package de.uni.trier.infsec.environment;
 
 
 public class AMTEnv {
-	public static void register(int id)	{
+	public static boolean registerSender(int id)	{
 		Environment.untrustedOutput(7801);
 		Environment.untrustedOutput(id);
-	}
-
-	public static boolean channelTo(int sender_id, int recipient_id, String server, int port) {
-		Environment.untrustedOutput(7802);
-		Environment.untrustedOutput(sender_id);
-		Environment.untrustedOutput(recipient_id);
-		Environment.untrustedOutputString(server);
-		Environment.untrustedOutput(port);
 		return Environment.untrustedInput()==0;
 	}
 
-	public static byte[] send(byte[] message, int sender_id, int recipient_id, String server, int port) {
+	public static byte[] sendTo(byte[] message, int sender_id, int recipient_id, String server, int port) {
 		Environment.untrustedOutput(7803);
 		Environment.untrustedOutputMessage(message);
 		Environment.untrustedOutput(sender_id);
